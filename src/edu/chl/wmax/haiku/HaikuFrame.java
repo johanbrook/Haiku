c73bade60f2e5979c34ef3202e6dcca4011dd1b4
kirayatail <kirayatail@gmail.com>
2012-02-28 00:21:57 +0100

    First working application will be a simple JFrame with three textFields, a dynamic label and a button.

---
+++ src/edu/chl/wmax/haiku/HaikuFrame.java
@@ -0,0 +1,16 @@
+package edu.chl.wmax.haiku;
+
+import javax.swing.JFrame;
+
+public class HaikuFrame extends JFrame {
+	
+	public HaikuFrame(){
+		
+	}
+	
+	public static void main(String[] args){
+		
+	}
+	
+	
+}
